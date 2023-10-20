e74b12b446f360ddbaf21f97777028ef3e7db599
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-10-20 10:43:47 -0300

    fix 7

--- sqlserver.ts
+++ sqlserver.ts
@@ -10,6 +10,7 @@ const pool = new ConnectionPool({
     database: process.env.DATABASE,
     port: Number(process.env.PORT_DATABASE),
     options: {
+        enableArithAbort: true,
         encrypt: true, // Use isso se você estiver usando o Azure
     },
 });
@@ -19,19 +20,6 @@ async function initializePool() {
     try {
         await pool.connect();
         console.log('Conexão com o banco de dados estabelecida.');
-
-        // Configurar o "keep alive" a cada 30 minutos (em milissegundos)
-        const keepAliveInterval = 30 * 60 * 1000; // 30 minutos
-        setInterval(async () => {
-            try {
-                // Execute uma consulta simples para manter a conexão ativa
-                const request = pool.request();
-                await request.query('SELECT 1');
-                console.log('Keep alive executado com sucesso.');
-            } catch (error) {
-                console.error('Erro no keep alive:', error);
-            }
-        }, keepAliveInterval);
     } catch (error) {
         console.error('Erro ao conectar ao banco de dados:', error);
     }
