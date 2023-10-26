35118d76df5e1a2aa7b237706c4b90fbf4712752
GuilhermeSSx <guilhermedosantos45@gmail.com>
2023-10-26 00:28:46 -0300

    correção n#1

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000, https://jpnr-gestao.vercel.app/, https://jpnr-gestao-guilhermessx.vercel.app/, https://jpnr-gestao-git-main-guilhermessx.vercel.app/ ");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4000, https://jpnr-gestao-sqlserver.vercel.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
