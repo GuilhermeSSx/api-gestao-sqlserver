95ce349bb9729b7f072621ec79f97810adb22511
GuilhermeSSx <guilhermedosantos45@gmail.com>
2025-09-02 07:33:03 -0300

    CORS fix javascript dist

--- dist/server.js
+++ dist/server.js
@@ -8,16 +8,15 @@ const user_routes_1 = require("./routes/user.routes");
 const cadastros_routes_1 = require("./routes/cadastros.routes");
 const favorecidos_routes_1 = require("./routes/favorecidos.routes");
 const dotenv_1 = require("dotenv");
+const cors_1 = __importDefault(require("cors"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
-const cors = require('cors');
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-});
-app.use(cors());
+const corsOptions = {
+    origin: process.env.ALLOWED_ORIGIN,
+    optionsSuccessStatus: 200,
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use((0, cors_1.default)(corsOptions)); // Apenas a configuração com o pacote cors()
 app.use(express_1.default.json());
 app.use('/user', user_routes_1.userRoutes);
 app.use('/cadastros', cadastros_routes_1.cadastrosRoutes);
@@ -25,7 +24,7 @@ app.use('/favorecidos', favorecidos_routes_1.favorecidosRoutes);
 app.get('/', (req, res) => {
     res.send('Bem-vindo à API de Gestão!');
 });
-//criar o servidor
-app.listen(4000, function () {
-    console.log("[ server start : port 4000 - OK ]");
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, function () {
+    console.log(`[ server start : port ${PORT} - OK ]`);
 });
