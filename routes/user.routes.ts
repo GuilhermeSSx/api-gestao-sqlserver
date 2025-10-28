e823201946755bb7709ec0730c4df6ad563e7487
GuilhermeSSx <guilhermedosantos45@gmail.com>
2025-10-28 14:01:52 -0300

    --

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -22,34 +22,6 @@ userRoutes.delete('/delete-user/:id', login, (request, response) => {
     userRepository.deleteUser(request, response);
 })
 
-userRoutes.post('/criar-perfil-acesso', login, (request, response) => {
-    userRepository.criarPerfilAcesso(request, response);
-})
-
-userRoutes.get('/get-perfil-acessos', login, (request, response) => {
-    userRepository.getPerfilAcessos(request, response);
-})
-
-userRoutes.get('/get-perfil-acessos-usuario', login, (request, response) => {
-    userRepository.getPerfilAcessosUsuario(request, response);
-})
-
-userRoutes.delete('/excluir-perfil-acesso', login, (request, response) => {
-    userRepository.excluirPerfilAcesso(request, response);
-})
-
-userRoutes.post('/get-perfil-acesso', login, (request, response) => {
-    userRepository.carregarPerfilAcesso(request, response);
-})
-
-userRoutes.put('/update-acesso-modulo', login, (request, response) => {
-    userRepository.updateAcessoModulo(request, response);
-})
-
-userRoutes.put('/update-acesso-funcionalidade', login, (request, response) => {
-    userRepository.updateAcessoFuncionalidade(request, response);
-})
-
 userRoutes.post('/get-usuario-filtrado', (request, response) => {
     userRepository.UsuariosFiltrados(request, response);
 })
