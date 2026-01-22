4b51474fe90918cea688b8810afd05ce8edf933e
GuilhermeSSx <guilhermedosantos45@gmail.com>
2026-01-22 10:23:37 -0300

    removido login middleware de consultar role_id para login

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -12,7 +12,7 @@ userRoutes.post('/sign-in', (req, res) => userRepository.login(req, res));
 // --- Rotas Privadas (Protegidas pelo middleware login) ---
 // Adicionei o login nestas duas rotas por segurança. Se precisarem ser públicas, pode remover.
 userRoutes.post('/get-usuario-filtrado', login, (req, res) => userRepository.UsuariosFiltrados(req, res));
-userRoutes.post('/consultar-role-id', login, (req, res) => userRepository.consultarRoleIdUsuario(req, res));
+userRoutes.post('/consultar-role-id', (req, res) => userRepository.consultarRoleIdUsuario(req, res));
 
 userRoutes.get('/get-users', login, (req, res) => userRepository.getUsers(req, res));
 userRoutes.delete('/delete-user/:id', login, (req, res) => userRepository.deleteUser(req, res));
